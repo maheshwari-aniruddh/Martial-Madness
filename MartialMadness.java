e9c09c87a49eafc1a263aef565eb8ea5f847c725
Aniruddh Maheshwari <aniruddhm.modi@gmail.com>
2026-06-06 09:18:17 -0700

    Restore original game view without practice arena

--- MartialMadness.java
+++ MartialMadness.java
@@ -405,7 +405,6 @@ class LevelPanelHolder extends JPanel
     private LevelPanel lp;
     private GamePanel tutorialGP;
     private GamePanel[] levelsGPs = new GamePanel[6];
-    private PracticeArenaPanel practiceArenaPanel;
 
     public LevelPanelHolder(MartialMadnessHolder mmhIn, CardLayout cardsIn, Information infoIn)
     {
@@ -438,23 +437,9 @@ class LevelPanelHolder extends JPanel
         {
             levelsGPs[i] = new GamePanel(mmh,cards,this,info,backGroundPicNames[i],levels[i]);
             add(levelsGPs[i],"GamePanel" + (i));
+    
         }
 
-        practiceArenaPanel = new PracticeArenaPanel(this, levelCards, info);
-        practiceArenaPanel.setOpaque(false);
-
-        JPanel practiceWrapper = new JPanel(new BorderLayout()) {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                Image bg = info.getMyImage("dojo.png");
-                if (bg != null) {
-                    g.drawImage(bg, 0, 0, getWidth(), getHeight(), this);
-                }
-            }
-        };
-        practiceWrapper.add(practiceArenaPanel, BorderLayout.CENTER);
-        add(practiceWrapper, "PracticeArena");
     }
 
     public void startTutorial()
@@ -469,13 +454,6 @@ class LevelPanelHolder extends JPanel
         levelsGPs[levelNum].initializeLevel();
         CardLayout cl = (CardLayout) getLayout();
         cl.show(this, "GamePanel" + levelNum);
-    }
-
-    public void startPracticeArena()
-    {
-        practiceArenaPanel.startArena();
-        CardLayout cl = (CardLayout) getLayout();
-        cl.show(this, "PracticeArena");
     }
 }
 
@@ -533,10 +511,6 @@ class LevelPanel extends JPanel
                 else if(command.equals("Extra Points"))
                 {
                     levelCards.show(lph,"ExtraCredit");
-                }
-                else if(command.equals("Practice Arena"))
-                {
-                    lph.startPracticeArena();
                 }
             }
 
@@ -596,10 +570,6 @@ class LevelPanel extends JPanel
         level7.setPreferredSize(new Dimension(150,100));
         level7.addActionListener(bh);
 
-        JButton level8 = new JButton("Practice Arena");
-        level8.setPreferredSize(new Dimension(150,100));
-        level8.addActionListener(bh);
-
         centerPanel.add(level0);
         centerPanel.add(level1);
         centerPanel.add(level2);
@@ -608,7 +578,6 @@ class LevelPanel extends JPanel
         centerPanel.add(level5);
         centerPanel.add(level6);
         centerPanel.add(level7);
-        centerPanel.add(level8);
         add(centerPanel,BorderLayout.CENTER);
 
         FixedPanelHolder fph = new FixedPanelHolder(mmh,cards,false,lph);
